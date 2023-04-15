d07a9961dc6bd00f4a19afed9165631cdb8b162f
bubuding0809 <bubuding0809@gmail.com>
2023-04-15 15:02:11 +0800

    Changed port to 443

--- index.ts
+++ index.ts
@@ -7,7 +7,8 @@ import StarterKit from "@tiptap/starter-kit";
 const prisma = new PrismaClient();
 
 const server = Server.configure({
-  port: 1234,
+  // Make the server available over https
+  port: 443,
   debounce: 500,
   onStoreDocument: async ({ documentName, document }) => {
     // Extract the task id from the document name
