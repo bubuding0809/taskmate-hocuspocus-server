6213070607be0aad0a42c0be5ea65630dbc770f3
bubuding0809 <bubuding0809@gmail.com>
2023-04-17 14:06:02 +0800

    Added connect and disconnect callbacks

--- index.ts
+++ index.ts
@@ -10,6 +10,8 @@ const server = Server.configure({
   // Make the server available over https
   port: 1234,
   debounce: 500,
+
+  // Callback to save the document to DB
   onStoreDocument: async ({ documentName, document }) => {
     // Extract the task id from the document name
     const taskId = documentName.split(".")[1];
@@ -26,6 +28,8 @@ const server = Server.configure({
       },
     });
   },
+
+  // Callback to initialize the document by loading it from DB
   onLoadDocument: async ({ documentName }) => {
     // Extract the task id from the document name
     const taskId = documentName.split(".")[1];
@@ -46,6 +50,16 @@ const server = Server.configure({
     return task.task_description !== null
       ? TiptapTransformer.toYdoc(task.task_description, "default", [StarterKit])
       : new Doc();
+  },
+
+  // Callback to handle client connection to document
+  onConnect: async ({ documentName }) => {
+    console.log("Client connected to document: ", documentName);
+  },
+
+  // Callback to handle client disconnection from document
+  onDisconnect: async ({ documentName }) => {
+    console.log("Client disconnected from document: ", documentName);
   },
 });
 
