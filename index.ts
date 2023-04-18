154a38be4b263c93a7e03e2190f84e0a2fae934f
bubuding0809 <bubuding0809@gmail.com>
2023-04-18 17:03:58 +0800

    Added typing for context

--- index.ts
+++ index.ts
@@ -1,44 +1,51 @@
-import { Server } from "@hocuspocus/server";
+import {
+  Server,
+  onDisconnectPayload,
+  onLoadDocumentPayload,
+  onStoreDocumentPayload,
+} from "@hocuspocus/server";
 import { PrismaClient } from "@prisma/client";
 import { Doc } from "yjs";
 import { TiptapTransformer } from "@hocuspocus/transformer";
 import StarterKit from "@tiptap/starter-kit";
 
+import type { User } from "@prisma/client";
+
 const prisma = new PrismaClient();
 
 const server = Server.configure({
-  // Make the server available over https
   port: 1234,
   debounce: 500,
 
   // Callback to handle authentication
   onAuthenticate: async ({ token }) => {
-    console.log("Authenticating user: ", token);
-
-    // Validate the token
+    // Validate the token against the database
     const user = await prisma.user.findUnique({
       where: {
         id: token,
       },
     });
 
-    // Return null if the user does not exist
+    // Throw an error if the user does not exist
     if (!user) {
       throw new Error("User does not exist, you are not allowed to connect");
     }
 
-    // Return the user object
+    console.log(`User ${user.name} authenticated with ${user.email}`);
     return user;
   },
 
   // Callback to save the document to DB
-  onStoreDocument: async ({ documentName, document, context }) => {
-    console.log(context);
-
+  onStoreDocument: async ({
+    documentName,
+    document,
+    context: user,
+  }: Omit<onStoreDocumentPayload, "context"> & {
+    context: User;
+  }) => {
     // Extract the task id from the document name
     const taskId = documentName.split(".")[1];
 
-    console.log("Storing task description for task: ", taskId);
     // Store the task description
     await prisma.task.update({
       where: {
@@ -49,26 +56,35 @@ const server = Server.configure({
         updated_at: new Date(),
       },
     });
+
+    console.log(
+      `Changes to document saved to DB: ${documentName} by ${user.name}`
+    );
   },
 
   // Callback to initialize the document by loading it from DB
-  onLoadDocument: async ({ documentName, context }) => {
-    console.log(context);
+  onLoadDocument: async ({
+    documentName,
+    context: user,
+  }: Omit<onLoadDocumentPayload, "context"> & {
+    context: User;
+  }) => {
     // Extract the task id from the document name
-    const taskId = documentName.split(".")[1];
+    const documentId = documentName.split(".")[1];
 
-    console.log("Loading task: ", taskId);
     // Fetch the task from the database
     const task = await prisma.task.findUnique({
       where: {
-        id: taskId,
+        id: documentId,
       },
     });
 
     // Return null if the task does not exist
     if (!task) {
-      return null;
+      throw new Error("Document does not exist");
     }
+
+    console.log(`Document loaded from DB: ${documentName} by ${user.name}`);
 
     return task.task_description !== null
       ? TiptapTransformer.toYdoc(task.task_description, "default", [StarterKit])
@@ -76,14 +92,20 @@ const server = Server.configure({
   },
 
   // Callback to handle client connection to document
-  onConnect: async ({ documentName, connection }) => {
-    console.log("Client connected to document: ", documentName);
+  onConnect: async ({ documentName }) => {
+    console.log("Client connecting to document: ", documentName);
   },
 
   // Callback to handle client disconnection from document
-  onDisconnect: async ({ documentName, context }) => {
-    console.log(context);
-    console.log("Client disconnected from document: ", documentName);
+  onDisconnect: async ({
+    documentName,
+    context: user,
+  }: Omit<onDisconnectPayload, "context"> & {
+    context: User;
+  }) => {
+    console.log(
+      `Client disconnected from document: ${documentName} by ${user.name}`
+    );
   },
 });
 
